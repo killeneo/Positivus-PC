feca8d787a4beecf146bdc4afa72940673af5582
Mr.ShReCk <penimetno@gmail.com>
2024-11-05 17:17:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const ac = document.getElementById("headingOne")
+const ac1 = document.getElementById("headingTwo")
+const ac2 = document.getElementById("headingThree")
+const ac3 = document.getElementById("headingFour")
+const ac4 = document.getElementById("headingFive")
+const ac5 = document.getElementById("headingSix")
+ac.onclick = () =>{
+    ac.style.backgroundColor = " #B9FF66"
+}
+ac1.onclick = () =>{
+    ac1.style.backgroundColor = " #B9FF66"
+}
+ac2.onclick = () =>{
+    ac2.style.backgroundColor = " #B9FF66"
+}
+ac3.onclick = () =>{
+    ac3.style.backgroundColor = " #B9FF66"
+}
+ac4.onclick = () =>{
+    ac4.style.backgroundColor = " #B9FF66"
+}
+ac5.onclick = () =>{
+    ac5.style.backgroundColor = " #B9FF66"
+}
